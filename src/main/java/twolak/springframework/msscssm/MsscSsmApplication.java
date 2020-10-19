c428585c4c15e72ba2ca7e9b897241eb7a9608e8
twolak88 <twolak88@gmail.com>
2020-10-19 23:07:17 +0200

    Create initial project. Closes #1

---
+++ src/main/java/twolak/springframework/msscssm/MsscSsmApplication.java
@@ -0,0 +1,12 @@
+package twolak.springframework.msscssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscSsmApplication.class, args);
+    }
+}
---
+++ src/test/java/twolak/springframework/msscssm/MsscSsmApplicationTests.java
@@ -0,0 +1,12 @@
+package twolak.springframework.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
