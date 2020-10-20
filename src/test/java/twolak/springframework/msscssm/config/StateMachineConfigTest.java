a481cdbc24f58f39b5b03b03c276138663f67e64
twolak88 <twolak88@gmail.com>
2020-10-20 22:51:49 +0200

    Transition Configuration. Example Unit test. Closes #5

---
+++ src/test/java/twolak/springframework/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,67 @@
+package twolak.springframework.msscssm.config;
+
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import twolak.springframework.msscssm.domain.PaymentEvent;
+import twolak.springframework.msscssm.domain.PaymentState;
+
+/**
+ *
+ * @author twolak
+ */
+@Slf4j
+@SpringBootTest
+public class StateMachineConfigTest {
+    
+    @Autowired
+    private StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+    
+    @Test
+    public void testNewStateMachine() throws Exception {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = stateMachineFactory.getStateMachine(UUID.randomUUID());
+        stateMachine.start();
+        
+        log.debug(stateMachine.getState().toString());
+        Assertions.assertEquals(PaymentState.NEW, stateMachine.getState().getId());
+        
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+        
+        log.debug(stateMachine.getState().toString());
+        Assertions.assertEquals(PaymentState.NEW, stateMachine.getState().getId());
+        
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+        
+        log.debug(stateMachine.getState().toString());
+        Assertions.assertEquals(PaymentState.PRE_AUTH, stateMachine.getState().getId());
+        
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTH_DECLINED);
+
+        log.debug(stateMachine.getState().toString());
+        Assertions.assertEquals(PaymentState.PRE_AUTH_ERROR, stateMachine.getState().getId());
+    }
+    
+    @Test
+    public void testNewStateMachinePreAuthDeclined() throws Exception {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = stateMachineFactory.getStateMachine(UUID.randomUUID());
+        stateMachine.start();
+
+        log.debug(stateMachine.getState().toString());
+        Assertions.assertEquals(PaymentState.NEW, stateMachine.getState().getId());
+
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+
+        log.debug(stateMachine.getState().toString());
+        Assertions.assertEquals(PaymentState.NEW, stateMachine.getState().getId());
+
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTH_DECLINED);
+
+        log.debug(stateMachine.getState().toString());
+        Assertions.assertEquals(PaymentState.PRE_AUTH_ERROR, stateMachine.getState().getId());
+    }
+}
